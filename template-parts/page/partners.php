da03f39ad12b3d51188ce924f5633a7c0a5415db
Markus <markus.uusitalo@hotmail.fi>
2020-12-14 23:54:54 +0200

    siirretty partnerissa if-condition aside.partners-säiliön ulkopuolelle

--- template-parts/page/partners.php
+++ template-parts/page/partners.php
@@ -1,8 +1,12 @@
+<?php
+include_once(ABSPATH.'wp-admin/includes/plugin.php');
+if (is_plugin_active('logo-slider-wp/logo-slider-wp.php')) {
+	?>
 <aside class="partners">
 	<h2><?= __("Partnered with", 'indecsdomain')?></h2>
 	<?php
-	include_once(ABSPATH.'wp-admin/includes/plugin.php');
-	if (is_plugin_active("logo-slider-wp/logo-slider-wp.php")) {
-		echo do_shortcode('[logo-slider]');
-	} ?>
+	echo do_shortcode('[logo-slider]'); ?>
 </aside>
+
+<?php
+}
