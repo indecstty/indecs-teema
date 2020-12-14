dcc5540885bb8454c14be5b213837c5a4439748c
Markus <markus.uusitalo@hotmail.fi>
2020-12-14 03:47:28 +0200

    lisätty aktiivisen slider-pluginin tarkistus

--- front-page.php
+++ front-page.php
@@ -23,7 +23,11 @@ $portal_bg = ' style="background: url(' . get_header_image() . ') no-repeat left
 	</main>
 	<aside class="partners">
 		<h2><?php _e('Partnered with', 'indecsdomain')?></h2>
-		<?php echo do_shortcode('[logo-slider]'); ?>
-	</aside>
+		<?php
+		include_once(ABSPATH.'wp-admin/includes/plugin.php');
+		if (is_plugin_active("logo-slider-wp/logo-slider-wp.php")) {
+					echo do_shortcode('[logo-slider]');
+		}
+	?></aside>
 
 <?php get_footer();
