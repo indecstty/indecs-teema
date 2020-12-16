c1fcf55b78f368451367f28209948f4aa3ada308
Markus <markus.uusitalo@hotmail.fi>
2020-12-16 19:57:52 +0200

    fade-in yritetty saada toimimaan

--- template-parts/page/partners.php
+++ template-parts/page/partners.php
@@ -2,7 +2,7 @@
 include_once(ABSPATH.'wp-admin/includes/plugin.php');
 if (is_plugin_active('logo-slider-wp/logo-slider-wp.php')) {
 	?>
-<aside class="partners">
+<aside class="partners fade-in">
 	<h2><?= __("Partnered with", 'indecsdomain')?></h2>
 	<?php
 	echo do_shortcode('[logo-slider]'); ?>
